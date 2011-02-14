936e7fd02b397cb4cf750a2fc5d2dd1b56fd1c3e
New2 <new2everything@gmail.com>
2011-02-14 14:06:12 -0500

    Fixed Post view error

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '3f9312a0c584211e4e6b10179f501b8c1f89619f827480b054522a74d44de809232b50a8efb9296ca1fc631574c9e45f60d2e5912eb5b6fbfa04ea707628e21f'
